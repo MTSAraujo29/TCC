d44dc9c1b7ba025df2f07edb11140bd816a1c29d
Seu Nome <seu-email@exemplo.com>
2025-07-04 13:17:28 -0300

    nbvhf

--- backend/api/index.js
+++ backend/api/index.js
@@ -212,7 +212,7 @@ app.post('/api/login', authLimiter, [
         }
 
         const token = jwt.sign({ userId: user.id, email: user.email },
-            JWT_SECRET, { expiresIn: '1m' }
+            JWT_SECRET, { expiresIn: '10m' }
         );
 
         res.json({
